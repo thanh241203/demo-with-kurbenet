5383086a1a7f0263eb86cb6aa5bd7342c4d43838
thanh241203 <thanhkon241203@gmail.com>
2025-11-05 14:43:41 +0700

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping()
+    public String sayHello2() {
+        return  "Hellop ";
+    }
+
+    @GetMapping("/get-all")
+    public String sayHello1() {
+        return  "dang o day";
+    }
+}
---
+++ demo/src/main/java/com/example/demo/DemoDeployKurbenetApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDeployKurbenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDeployKurbenetApplication.class, args);
+	}
+
+}
