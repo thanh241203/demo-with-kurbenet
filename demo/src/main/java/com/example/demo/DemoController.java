48ff4a11b2af8582bd8ce9af8526e6eed12a4023
thanhnv <thanhnv@ftlvn.com.vn>
2025-11-05 15:27:08 +0700

    check

--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping()
     public String sayHello2() {
-        return  "Hellop ";
+        return  "Hellop thanh";
     }
 
     @GetMapping("/get-all")
